0c93856f80447c175c5fd7a5f573b07f8b87dbb4
markmatney <mmatney@ucla.edu>
2016-02-24 00:39:06 -0800

    Bug fixes, hide unused filters, change color scheme, update about section

--- app/app.js
+++ app/app.js
@@ -9,25 +9,42 @@ app.controller('theList', ['$scope', '$http', function($scope, $http) {
       $scope.shows = res.data['data'];
     });
   $scope.lineupString = function(arr) { return arr.join(', '); };
+  $scope.lineupSearch = '';
+  $scope.venueSearch = '';
   $scope.dayFilter = {
-	  'SUN': false,
-	  'MON': false, 
-	  'TUE': false,
-	  'WED': false,
-	  'THU': false,
-	  'FRI': true,
-	  'SAT': true
+      'SUN': false,
+      'MON': false, 
+      'TUE': false,
+      'WED': false,
+      'THR': false,
+      'FRI': true,
+      'SAT': true
   };
   $scope.filterByDay = function(a) {
-	  // return true if Day of a is selected in dayfilter
-	  var day = a.when[0];
-	  if ($scope.dayFilter[day]) {
-		  return true;
-	  }
-	  else
-	  {
-		  return false;
-	  }
+      // return true if Day of a is selected in dayfilter
+      var day = a.when[0];
+      if ($scope.dayFilter[day]) {
+          return true;
+      }
+      else
+      {
+          return false;
+      }
   };
-
+  $scope.lineupFilter = function(a) {
+      // if search text is a substring in the artist list, retu true
+      var i;
+      for (i in a.who) {
+          if (a.who[i].toLowerCase().indexOf($scope.lineupSearch) != -1) {
+              return true;
+          }
+      }
+      return false;
+  }
+  $scope.venueFilter = function(a) {
+      if (a.where.toLowerCase().indexOf($scope.venueSearch) != -1) {
+          return true;
+      }
+      return false;
+  }
 }]);
