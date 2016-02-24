5f9d813df22f22bef631454859d2823a4fba0f14
markmatney <mmatney@ucla.edu>
2016-02-23 23:01:01 -0800

    Implement day of week filtering

--- app/app.js
+++ app/app.js
@@ -9,5 +9,25 @@ app.controller('theList', ['$scope', '$http', function($scope, $http) {
       $scope.shows = res.data['data'];
     });
   $scope.lineupString = function(arr) { return arr.join(', '); };
+  $scope.dayFilter = {
+	  'SUN': false,
+	  'MON': false, 
+	  'TUE': false,
+	  'WED': false,
+	  'THU': false,
+	  'FRI': true,
+	  'SAT': true
+  };
+  $scope.filterByDay = function(a) {
+	  // return true if Day of a is selected in dayfilter
+	  var day = a.when[0];
+	  if ($scope.dayFilter[day]) {
+		  return true;
+	  }
+	  else
+	  {
+		  return false;
+	  }
+  };
 
 }]);
