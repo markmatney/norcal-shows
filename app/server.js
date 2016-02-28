d064c5bdedc63bc7b0e00ed2a4398e2a36501925
markmatney <mmatney@ucla.edu>
2016-02-28 02:46:56 -0800

    get angular and font-awesome with npm

--- app/server.js
+++ app/server.js
@@ -2,4 +2,6 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
 app.use('/snapjs', express.static(__dirname + '/../node_modules/snapjs/'));
+app.use('/angular', express.static(__dirname + '/../node_modules/angular'));
+app.use('/font-awesome', express.static(__dirname + '/../node_modules/font-awesome'));
 app.listen(process.env.PORT || 3000);
