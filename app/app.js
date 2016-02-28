992ff5e573e57975bce7038df2fe9e48295a684d
markmatney <mmatney@ucla.edu>
2016-02-28 02:38:17 -0800

    style changes, update data source url

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ var app = angular.module('norcalShows', []);
 app.controller('theList', ['$scope', '$http', function($scope, $http) {
 
   // get JSON of shows
-  $http.get('http://bay-area-shows-api.herokuapp.com/v0.1/shows/')
+  $http.get('https://norcal-shows-data.herokuapp.com/v0.1/shows/')
     .then(function(res) {
       $scope.shows = res.data['data'];
     });
